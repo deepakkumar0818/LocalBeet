16d32bdacf6a6121e0c545e4c27b411d1250edce
amit helios tech <amit.heliostech@gmail.com>
2025-10-08 11:30:19 +0530

    added urls

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://localhost:5000', 
+        target: 'https://localbeet.onrender.com', 
         changeOrigin: true,
         secure: true,
         configure: (proxy, _options) => {
